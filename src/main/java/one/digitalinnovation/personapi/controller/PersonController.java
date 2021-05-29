24856e8d02f025149e3f48b27d8f00341ca24b8f
luisnasc <luisbrunu@gmail.com>
2021-05-29 03:44:32 -0300

    Develop delete operation to Person

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class PersonController {
 	@GetMapping("/{id}")
 	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
 		return personService.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+		personService.delete(id);
 	}
 	
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -47,8 +47,10 @@ public class PersonService {
 
 	public PersonDTO findById(Long id) throws PersonNotFoundException {
 		
-		Person person = personRepository.findById(id)
-			.orElseThrow(() -> new PersonNotFoundException(id));
+		//Person person = personRepository.findById(id)
+			//.orElseThrow(() -> new PersonNotFoundException(id));
+		
+		Person person = verifyIfExists(id);
 		
 //		Optional<Person> optionalPerson =  personRepository.findById(id);
 //		if(optionalPerson.isEmpty()) {
@@ -56,6 +58,18 @@ public class PersonService {
 //		}
 //		return personMapper.toDTO(optionalPerson.get());
 		return personMapper.toDTO(person);
+	}
+
+	public void delete(Long id) throws PersonNotFoundException {
+		verifyIfExists(id);
+		
+		personRepository.deleteById(id);
+		
+	}
+
+	private Person verifyIfExists(Long id) throws PersonNotFoundException {
+		return personRepository.findById(id)
+			.orElseThrow(() -> new PersonNotFoundException(id)  );
 	}
 		
 	
